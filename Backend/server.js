b27b0a4155aeba89e839824c6e30fed1fb8a4b74
AariyaTharshan <gamingdarkphoenix2@gmail.com>
2025-02-12 22:14:31 +0530

    Changed cors

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:5173',
-      'https://collab-code.vercel.app'
+      'https://collabcode-ide.vercel.app'
     ],
     methods: ["GET", "POST"],
     credentials: true
@@ -31,7 +31,7 @@ let totalLinesOfCode = 0;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://collab-code.vercel.app'
+    'https://collabcode-ide.vercel.app'
   ],
   credentials: true
 }));
